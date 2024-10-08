b4cb4a3678d20b723309d10204ab2d079c80b59e
Philix27 <philixbob@gmail.com>
2024-10-08 13:56:05 +0100

    fix import

--- app/(core)/search/page.tsx
+++ app/(core)/search/page.tsx
@@ -6,7 +6,8 @@ import { AppStores } from '@/lib';
 import { SearchInput } from '../_comps';
 import { chainRegistry } from '@/data';
 import { FaGithub, FaYoutube } from 'react-icons/fa';
-import { IoBrowsers } from 'react-icons/io';
+import { IoBrowsers } from 'react-icons/io5';
+
 
 export default function Page() {
   const store = AppStores.useSettings();
@@ -28,33 +29,35 @@ export default function Page() {
 
   return (
     <>
-      <div className={'w-full h-full py-4 px-4 flex flex-col items-center'}>
+      <div className={'w-full h-full py-4 px-4 flex flex-col items-center overflow-y-hidden'}>
         {/* <div className="w-[40%] mb-4"> */}
-        <SearchInput className="w-[40%] mb-4" />
-       
-        <div className="grid grid-cols-4 gap-2 w-full">
-          {getList().map((val, i) => (
-            <div
-              key={i}
-              className="w-full hover:bg-accent p-3 flex flex-col bg-secondary shadow-md"
-              onClick={() => {
-                store.update({ infoTabOpen: true, drawerIsOpen: false });
-              }}
-            >
-              <div className="flex gap-x-4 items-center justify-between mb-4 border-b pb-2">
-                <img src={val.logo} className="size-[30px] rounded-full" />
-                <TextP className=" font-bold text-xs">{val.name}</TextP>
+        {/* <SearchInput className="w-[40%] mb-4" /> */}
+
+        <div className="grid grid-cols-4 gap-2 w-full overflow-y-scroll scroll-smooth">
+          {getList()
+            .concat(getList(), getList())
+            .map((val, i) => (
+              <div
+                key={i}
+                className="w-full hover:bg-accent p-3 flex flex-col bg-secondary shadow-md"
+                onClick={() => {
+                  store.update({ infoTabOpen: true, drawerIsOpen: false });
+                }}
+              >
+                <div className="flex gap-x-4 items-center justify-between mb-4 border-b pb-2">
+                  <img src={val.logo} className="size-[30px] rounded-full" />
+                  <TextP className=" font-bold text-xs">{val.name}</TextP>
+                </div>
+                <TextP>{val.shortIntro}</TextP>
+                <TextP>{val.ecosystem}</TextP>
+                <TextP>{val.taxon}</TextP>
+                <div className="flex space-x-2 mt-2">
+                  <FaGithub size={18} className="text-slate-200" />
+                  <FaYoutube size={18} className="text-red-500" />
+                  <IoBrowsers size={18} className="text-blue-500" />
+                </div>
               </div>
-              <TextP>{val.shortIntro}</TextP>
-              <TextP>{val.ecosystem}</TextP>
-              <TextP>{val.taxon}</TextP>
-              <div className='flex space-x-2'>
-                <FaGithub />
-                <FaYoutube />
-                <IoBrowsers />
-              </div>
-            </div>
-          ))}
+            ))}
         </div>
       </div>
     </>
